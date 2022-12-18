c236f54a3e1481256f45588f360e67da52e53a84
BiiiiigQ <120688947+BiiiiigQ@users.noreply.github.com>
2022-12-18 17:56:16 -0500

    Update EASY 2-181 Combine Two Tables.sql

--- EASY 2-181 Combine Two Tables.sql
+++ EASY 2-181 Combine Two Tables.sql
@@ -1,9 +1,25 @@
-
-
-
+--ANSWER 1: LEFT OUTER JOIN 
 
 SELECT emp.name AS Employee
 FROM Employee emp
     LEFT OUTER JOIN Employee mag
                  ON emp.managerId = mag.id
 WHERE emp.salary > mag.salary
+
+
+-- ANSWER 2: SUBQUERY (1)
+
+SELECT emp.name AS Employee
+FROM Employee emp
+WHERE emp.salary > (SELECT mag.salary
+                    FROM Employee mag
+                    WHERE emp.managerId = mag.id)
+
+
+-- ANSWER 2: SUBQUERY (2)
+
+SELECT emp.name AS Employee
+FROM Employee emp
+WHERE EXISTS (SELECT 1
+              FROM Employee mag
+              WHERE emp.managerId = mag.id AND emp.salary > mag.salary)
