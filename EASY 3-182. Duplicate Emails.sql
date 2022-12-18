c70a0ad774dd1722e3fe54c0f49f4e90e8d3c188
BiiiiigQ <120688947+BiiiiigQ@users.noreply.github.com>
2022-12-18 18:09:07 -0500

    Update EASY 3-182. Duplicate Emails.sql

--- EASY 3-182. Duplicate Emails.sql
+++ EASY 3-182. Duplicate Emails.sql
@@ -1,1 +1,33 @@
+-- ANSWER 1: GROUP BY
 
+SELECT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) >= 2
+
+
+-- ANSWER 2: LEFT OUTER JOIN
+
+SELECT DISTINCT p1.email AS Email
+FROM Person p1
+    LEFT OUTER JOIN Person p2
+                 ON p1.email = p2.email AND p1.id <> p2.id
+WHERE p2.id IS NOT NULL
+
+
+
+-- ANSWER 3: INNER JOIN
+
+SELECT DISTINCT p1.email AS Email
+FROM Person p1
+    INNER JOIN Person p2
+            ON p1.email = p2.email AND p1.id <> p2.id
+
+
+-- ANSWER4: SUBQUERY
+
+SELECT DISTINCT p1.email AS Email
+FROM Person p1
+WHERE EXISTS (SELECT 1
+              FROM Person p2
+              WHERE p1.email = p2.email AND p1.id <> p2.id)
