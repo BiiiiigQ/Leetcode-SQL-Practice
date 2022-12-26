2a87bbc4ddfb7ac8ca2978b5848bead0b18cb343
BiiiiigQ <120688947+BiiiiigQ@users.noreply.github.com>
2022-12-26 17:14:48 -0500

    Update EASY 1-175 Combine Two Tables.sql

--- EASY 1-175 Combine Two Tables.sql
+++ EASY 1-175 Combine Two Tables.sql
@@ -9,3 +9,12 @@ FROM Person p
     LEFT OUTER JOIN Address a
                  ON p.personId = a.personId
 
+
+SELECT firstName, lastName,
+       (SELECT city
+        FROM Address a
+        WHERE p.personId = a.personId) AS city,
+       (SELECT state
+        FROM Address a
+        WHERE p.personId = a.personId) AS state
+FROM Person p
